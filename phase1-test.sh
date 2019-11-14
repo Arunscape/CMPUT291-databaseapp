12ac7429960a39adc727a37ac78dfa241e6e2f13
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-14 14:12:05 -0700

    fix shell-check warnings

--- phase1-test.sh
+++ phase1-test.sh
@@ -3,30 +3,33 @@
 set -e
 set -u
 
+# For testing only, do not submit
+
 function do_diff {
     echo '----- ----- -----'
     set +e
-    diff -u $1 $2
+    diff -u "$1" "$2"
     set -e
     echo '----- ----- -----'
 }
 
 function test_one {
     local PREFIX=./data${1}/${1}
-    python3 phase1.py < ${PREFIX}.xml
+    python3 phase1.py < "${PREFIX}".xml
 
     echo 'terms.txt diff:'
-    do_diff terms.txt ${PREFIX}-terms.txt
+    do_diff terms.txt "${PREFIX}"-terms.txt
 
     echo 'emails.txt diff:'
-    do_diff emails.txt ${PREFIX}-emails.txt
+    do_diff emails.txt "${PREFIX}"-emails.txt
 
     echo 'dates.txt diff:'
-    do_diff dates.txt ${PREFIX}-dates.txt
+    do_diff dates.txt "${PREFIX}"-dates.txt
 
     echo 'recs.txt diff:'
-    do_diff recs.txt ${PREFIX}-recs.txt
+    do_diff recs.txt "${PREFIX}"-recs.txt
 
+    # shellcheck disable=SC2162
     read -p 'Press enter to continue...'
 }
 
