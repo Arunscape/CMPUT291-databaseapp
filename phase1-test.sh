54a7d4ba80723263bfcf830c809f283a7a1d0a31
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-14 13:57:32 -0700

    update shell script

--- phase1-test.sh
+++ phase1-test.sh
@@ -4,26 +4,30 @@ set -e
 set -u
 
 function do_diff {
+    echo '----- ----- -----'
     set +e
     diff -u $1 $2
     set -e
+    echo '----- ----- -----'
 }
 
 function test_one {
     local PREFIX=./data${1}/${1}
     python3 phase1.py < ${PREFIX}.xml
 
-    echo "terms.txt diff:"
+    echo 'terms.txt diff:'
     do_diff terms.txt ${PREFIX}-terms.txt
 
-    echo "emails.txt diff:"
+    echo 'emails.txt diff:'
     do_diff emails.txt ${PREFIX}-emails.txt
 
-    echo "dates.txt diff:"
+    echo 'dates.txt diff:'
     do_diff dates.txt ${PREFIX}-dates.txt
 
-    echo "recs.txt diff:"
+    echo 'recs.txt diff:'
     do_diff recs.txt ${PREFIX}-recs.txt
+
+    read -p 'Press enter to continue...'
 }
 
 test_one 10
