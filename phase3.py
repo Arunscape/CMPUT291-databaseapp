26dd8e180b59f7158acb56d0533d79154c22c76d
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-18 10:41:14 -0700

    phase 3 start

--- phase3.py
+++ phase3.py
@@ -1,1 +1,21 @@
 #! /usr/bin/env python3
+
+from bsddb3 import db
+
+DB = db.DB()
+
+DB.open('terms.txt', None, db.DB_BTREE, db.DB_CREATE)
+DB.open('emails.txt', None, db.DB_BTREE, db.DB_CREATE)
+DB.open('dates.txt', None, db.DB_BTREE, db.DB_CREATE)
+DB.open('recs.txt', None, db.DB_BTREE, db.DB_CREATE)
+
+CUR = DB.cursor()
+
+it = cur.first
+while it:
+    print(it)
+
+
+
+CUR.close()
+DB.close()
