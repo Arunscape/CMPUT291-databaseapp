1e9a51cf5df40e3abbbc91d42f7d6ff05eddcf2f
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-18 10:02:00 -0700

    rm -f to supress error if file does not exist

--- phase2.sh
+++ phase2.sh
@@ -3,10 +3,10 @@
 set -e
 set -u
 
-rm re.idx
-rm te.idx
-rm em.idx
-rm da.idx
+rm -f re.idx
+rm -f te.idx
+rm -f em.idx
+rm -f da.idx
 
 sort -u recs.txt | ./break.pl | db_load -T -t hash re.idx
 sort -u terms.txt | ./break.pl | db_load -c duplicates=1 -T -t btree te.idx
