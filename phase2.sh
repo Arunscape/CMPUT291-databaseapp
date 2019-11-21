0f06d753c5d01e4c1d48e79e12ba57b1784ccaaa
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-21 13:06:16 -0700

    make phase2 run in parallel to make it faster

--- phase2.sh
+++ phase2.sh
@@ -6,9 +6,11 @@ set -u
 rm -f re.idx
 rm -f te.idx
 rm -f em.idx
-rm -f da.idx
+rm -f da.idx 
 
-sort -u recs.txt | ./break.pl | db_load -T -t hash re.idx
-sort -u terms.txt | ./break.pl | db_load -c duplicates=1 -T -t btree te.idx
-sort -u emails.txt | ./break.pl | db_load -c duplicates=1 -T -t btree em.idx
-sort -u dates.txt | ./break.pl | db_load -c duplicates=1 -T -t btree da.idx
+sort -u recs.txt | ./break.pl | db_load -T -t hash re.idx &
+sort -u terms.txt | ./break.pl | db_load -c duplicates=1 -T -t btree te.idx &
+sort -u emails.txt | ./break.pl | db_load -c duplicates=1 -T -t btree em.idx &
+sort -u dates.txt | ./break.pl | db_load -c duplicates=1 -T -t btree da.idx &
+
+wait
