c7d59598f24a7f21f04c0e3e9790115f3675a6b9
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-21 14:01:53 -0700

    can parse email

--- phase3.py
+++ phase3.py
@@ -38,6 +38,11 @@ class DateQuery(Query):
     date_prefix: str
     date: str
 
+@dataclass
+class EmailQuery(Query):
+    email_prefix: str
+    email_address: str
+
 
 class ModeChange:
     pass
@@ -51,7 +56,12 @@ class Parser:
     def parse(self) -> Query:
         try:
             return self.dateQuery()
-        except DateParseException:
+        except DateParseException as e:
+            print(e)
+
+        try: 
+            return self.emailQuery()
+        except EmailParseException:
             pass
 
     def chomp(self) -> str:
@@ -80,7 +90,7 @@ class Parser:
         self.index += 4
 
         self.chomp_whitespace()
-        tok = self.chomp()
+        tok: str = self.chomp()
 
         if tok in (":", ">", "<", ">=", "<="):
             return self.string[: self.index]
@@ -95,11 +105,59 @@ class Parser:
         match = date_regex.search(self.string)
         if match is not None and match.start() == self.index:
             self.index = match.end()
+            # if there are extra characters
+            if not self.index == len(self.string):
+                raise DateParseException("Error: extra characters after date")
+        
             return self.string[match.start() : match.end()]
 
         raise DateParseException("Could not parse date")
-
     ############################################################################
+
+    #emailQuery
+    #emailterm	::= alphanumeric+ | alphanumeric+ '.' emailterm
+    #email		::= emailterm '@' emailterm
+    #emailQuery	::= emailPrefix whitespace* email
+    ############################################################################
+    def emailQuery(self) -> EmailQuery:
+        pre: str = self.emailPrefix()
+        self.chomp_whitespace()
+        email = self.email_address()
+
+        return EmailQuery(pre, email)
+
+
+    #emailPrefix ::= (from | to | cc | bcc) whitespace* ':'
+    def emailPrefix(self) -> str:
+        prefix: str
+        if self.string[:2] in ('cc', 'to'):
+            self.index += 2
+            prefix = self.string[:2]
+        elif self.string[:3] == 'bcc':
+            self.index += 3
+            prefix = 'bcc'
+        elif self.string[:4] == 'from':
+            self.index += 4
+            prefix = 'from'
+
+        self.chomp_whitespace()
+
+        if self.chomp() != ':':
+            raise EmailParseException("no colon ':' in emailPrefix")
+
+        return self.string[:self.index]
+
+    def email_address(self) -> str:
+        email_term_regex = re.compile(r"[0-9a-zA-Z_-]+@[0-9a-zA-Z_-]+\.[0-9a-zA-Z_-]+")
+
+        match = email_term_regex.search(self.string)
+        if match is not None and match.start() == self.index:
+            self.index = match.end()
+            # if there are extra characters
+            if not self.index == len(self.string):
+                raise EmailParseException("Error: extra characters after email")
+        
+            return self.string[match.start() : match.end()]
 
 
 class ParseException(Exception):
@@ -109,11 +167,17 @@ class ParseException(Exception):
 class DateParseException(ParseException):
     pass
 
+class EmailParseException(ParseException):
+    pass
+
 
 # test
-p = Parser("date    :     2000/11/11")
+#p = Parser("date:     2000/11/11")
+#idk = p.parse()
+#print(idk.date_prefix)
+#print(idk.date)
 
+p = Parser("from : abc@x.yz")
 idk = p.parse()
-
-print(idk.date_prefix)
-print(idk.date)
+print(idk.email_prefix)
+print(idk.email_address)
