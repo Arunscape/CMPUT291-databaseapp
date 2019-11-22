dff6d00940ac03d2bd4aaffee4154b71a44e1f19
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-22 11:54:54 -0700

    abandon recursive descent parser 😢

--- phase3/main.py
+++
@@ -1,8 +0,0 @@
-from parser import Parser
-
-
-line = input("Enter command")
-
-if line is not None:
-    p = Parser(line)
-    p.parse()
--- phase3/parser.py
+++ phase3/parser.py
@@ -1,291 +1,44 @@
-#! /usr/bin/env python3
-
-from dataclasses import dataclass
-from typing import List
-
 import re
 
-"""
-alphanumeric    ::= [0-9a-zA-Z_-]
-numeric		::= [0-9]
-date            ::= numeric numeric numeric numeric '/' numeric numeric '/' numeric numeric
-datePrefix      ::= 'date' whitespace* (':' | '>' | '<' | '>=' | '<=')
-dateQuery       ::= datePrefix whitespace* date
-emailterm	::= alphanumeric+ | alphanumeric+ '.' emailterm
-email		::= emailterm '@' emailterm
-emailPrefix	::= (from | to | cc | bcc) whitespace* ':'
-emailQuery	::= emailPrefix whitespace* email
-term            ::= alphanumeric+
-termPrefix	::= (subj | body) whitespace* ':'
-termSuffix      ::= '%' 
-termQuery       ::= termPrefix? whitespace* term termSuffix?
-
-expression      ::= dateQuery | emailQuery | termQuery 
-query           ::= expression (whitespace expression)*
-
-modeChange	::= 'output=full' | 'output=brief'
-
-command		::= query | modeChange
-"""
-
-
-class Query:
-    pass
-
-
-@dataclass
-class DateQuery(Query):
-    date_prefix: str
-    date: str
-
-
-@dataclass
-class EmailQuery(Query):
-    email_prefix: str
-    email_address: str
-
-
-@dataclass
-class TermQuery(Query):
-    term_prefix: str
-    term: str
-    suffix: bool  # True if wildcard '%' is present
-
-
-class Parser:
-    def __init__(self, s) -> None:
-        self.index = 0
-        self.string = s
-        self.queries: List[Query] = []
-
-    def parse(self) -> List[Query]:
-
-        if self.string == "output=full":
-            return None  # TODO change mode
-        elif self.string == "output=brief":
-            return None  # T ODO change mode
-        else:
-            try:
-                self.queries.append(self.dateQuery())
-                self.string = self.string[self.index :]
-            except DateParseException:
-                pass
-            finally:
-                self.index = 0
-
-            try:
-                self.queries.append(self.emailQuery())
-                self.string = self.string[self.index :]
-            except EmailParseException:
-                pass
-            finally:
-                self.index = 0
-
-            try:
-                self.queries.append(self.termQuery())
-                self.string = self.string[self.index :]
-            except TermParseException:
-                pass
-            finally:
-                self.index = 0
-
-            return self.queries
-
-    def chomp(self) -> str:
-        self.index += 1
-        return self.string[self.index - 1]
-
-    def chomp_whitespace(self) -> None:
-        try:
-            while self.string[self.index] == " ":
-                self.index += 1
-        except IndexError:
-            pass
-
-    ############################################################################
-    # dateQuery logic
-    # dateQuery ::= datePrefix whitespace* date
-    def dateQuery(self) -> DateQuery:
-        pre: str = self.datePrefix()
-        self.chomp_whitespace()
-        d: str = self.date()
-        return DateQuery(pre, d)
-
-    # datePrefix ::= 'date' whitespace* (':' | '>' | '<' | '>=' | '<=')
-    def datePrefix(self) -> str:
-
-        # chomp 'date'
-        if self.string[self.index : self.index + 4] != "date":
-            raise DateParseException("Could not parse datePrefix")
-        self.index += 4
-
-        self.chomp_whitespace()
-        tok: str = self.chomp()
-
-        if tok in (":", ">", "<"):
-            # covers >=, <=
-            if self.string[self.index + 1] == "=":
-                self.index += 1
-
-            return self.string[: self.index]
-
-        raise DateParseException("Could not parse datePrefix pt. 2")
-
-    def date(self) -> str:
-
-        self.chomp_whitespace()
-
-        date_regex = re.compile(r"\d{4}\/\d{2}\/\d{2}")
-        match = date_regex.search(self.string)
-        if match is not None and match.start() == self.index:
-            self.index = match.end()
-            self.chomp_whitespace()
-            return self.string[match.start() : match.end()]
-
-        raise DateParseException("Could not parse date")
-
-    ############################################################################
-
-    # emailQuery ::= emailPrefix whitespace* email
-    ############################################################################
-    def emailQuery(self) -> EmailQuery:
-        pre: str = self.emailPrefix()
-        self.chomp_whitespace()
-        email = self.email_address()
-
-        return EmailQuery(pre, email)
-
-    # emailPrefix ::= (from | to | cc | bcc) whitespace* ':'
-    def emailPrefix(self) -> str:
-        prefix: str
-        if self.string[self.index : self.index + 2] in ("cc", "to"):
-            self.index += 2
-            prefix = self.string[self.index : self.index + 2]
-        elif self.string[self.index : self.index + 3] == "bcc":
-            self.index += 3
-            prefix = "bcc"
-        elif self.string[self.index : self.index + 4] == "from":
-            self.index += 4
-            prefix = "from"
-
-        self.chomp_whitespace()
-
-        if self.chomp() != ":":
-            raise EmailParseException("no colon ':' in emailPrefix")
-
-        return self.string[: self.index]
-
-    # TODO it doesn't fully comply with this grammar yet
-    # emailterm	::= alphanumeric+ | alphanumeric+ '.' emailterm
-    # email ::= emailterm '@' emailterm
-    def email_address(self) -> str:
-
-        email_term_regex = re.compile(
-            r"[0-9a-zA-Z_-]+(?:\.[0-9a-zA-Z_-]+)*@[0-9a-zA-Z_-]+(?:\.[0-9a-zA-Z_-]+)*"
-        )
-
-        match = email_term_regex.search(self.string)
-        if match is not None and match.start() == self.index:
-            self.index = match.end()
-            self.chomp_whitespace()
-            return self.string[match.start() : match.end()]
-
-    ############################################################################
-
-    # termSuffix ::= '%'
-    # termQuery ::= termPrefix? whitespace* term termSuffix?
-    def termQuery(self) -> TermQuery:
-        pre: str
-        try:
-            pre = self.termPrefix()
-            self.chomp_whitespace()
-        except TermParseException:  # optional
-            pre = None
-
-        term: str = self.term()
-
-        suffix: bool = self.termSuffix()
-
-        return TermQuery(pre, term, suffix)
-
-    # termPrefix ::= (subj | body) whitespace* ':'
-    def termPrefix(self) -> str:
-
-        if self.string[self.index : self.index + 4] not in ("subj", "body"):
-            raise TermParseException()
-
-        self.index += 4
-        self.chomp_whitespace()
-
-        if self.chomp() != ":":
-            raise TermParseException("Colon not found at end of prefix")
-
-        return self.string[: self.index]
-
-    # term ::= alphanumeric+
-    def term(self) -> str:
-        term_regex = re.compile(r"[0-9a-zA-Z_-]+")
-
-        match = term_regex.search(self.string[self.index :])
-        print("REEEEE")
-        print(self.string[match.start() + self.index : match.end() + self.index])
-        print(self.index)
-        print("REEEE")
-        if match is not None and match.start() == 0:
-            offset = self.index
-            self.index += match.end() - match.start()
-            return self.string[match.start() + offset : match.end() + offset]
-
-        raise TermParseException("Cannot parse term")
-
-    def termSuffix(self) -> bool:
-
-        ret = False
-        if self.chomp() == "%":
-            ret = True
-
-        self.chomp_whitespace()
-
-        return ret
-
-
-class ParseException(Exception):
-    pass
-
-
-class DateParseException(ParseException):
-    pass
-
-
-class EmailParseException(ParseException):
-    pass
-
-
-class TermParseException(ParseException):
-    pass
-
-
-if __name__ == "__main__":
-    # test
-    # p = Parser("date:     2000/11/11")
-    # idk = p.parse()
-    # print(idk.date_prefix)
-    # print(idk.date)
-
-    # p = Parser("from : abc@x.yz")
-    # idk = p.parse()
-    # print(idk.email_prefix)
-    # print(idk.email_address)
-
-    # p = Parser("subj :  abcd33dde%")
-    # idk = p.parse()
-    # print(idk.term_prefix)
-    # print(idk.term)
-    # print(idk.suffix)
-
-    # p = Parser("date:     2000/11/11  subj :  abcd33dde%")
-    # idk = p.parse()
-    # print(p.queries)
-    # for x in idk:
-    #     print(x)
-    pass
+dateQuery = re.compile(r'^date\s*(:|>|<|>=|<=)\s*([0-9]{4}/[0-9]{2}/[0-9]{2})(?: |$)')
+emailQuery = re.compile(
+    r'^(from|to|cc|bcc)\s*:\s*([0-9a-zA-Z_-]+(?:\.[0-9a-zA-Z_-]+)*@[0-9a-zA-Z_-]+(?:\.[0-9a-zA-Z_-]+)*)(?: |$)')
+termQuery = re.compile(r'^(subj|body|)\s*([0-9a-zA-Z_-]+)(%|)(?: |$)')
+
+
+def parse(line):
+    while len(line) > 0:
+        print(line)
+
+        match = dateQuery.match(line)
+        if match is not None:
+            print("DATE(operator, date) = (", match.group(1), ", ", match.group(2), ")")
+            line = line[match.end():]
+            continue
+
+        match = emailQuery.match(line)
+        if match is not None:
+            print("EMAIL(field, email) = (", match.group(1), ", ", match.group(2), ")")
+            line = line[match.end():]
+            continue
+
+        match = termQuery.match(line)
+        if match is not None:
+            print("TERM(field?, term, end?) = (", match.group(1), ", ", match.group(2), ",", match.group(3), ")")
+            line = line[match.end():]
+            continue
+
+        print("Syntax error")
+        return
+
+    print("No syntax error, parsing complete")
+
+
+if __name__ == '__main__':
+    parse("date>1999/12/21")
+    print()
+    parse("from:joe@exmaple.com")
+    print()
+    parse("subj test%")
+    print()
+    parse("date  <= 1999/12/21 from     : joe.joe.joe.joe@localhost body hello date > 1888/01/01")
