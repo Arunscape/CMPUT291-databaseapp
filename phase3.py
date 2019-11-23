e9a299c7839b7349e7b39b854f1e6d72a24a4ffd
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-23 16:08:10 -0700

    refactor: global cursor

--- phase3.py
+++ phase3.py
@@ -13,9 +13,15 @@ def open_db(file, mode):
 
 
 recs_db = open_db("re.idx", db.DB_HASH)
+
 terms_db = open_db("te.idx", db.DB_BTREE)
+terms_curs = terms_db.cursor()
+
 emails_db = open_db("em.idx", db.DB_BTREE)
+emails_curs = emails_db.cursor()
+
 dates_db = open_db("da.idx", db.DB_BTREE)
+dates_curs = dates_db.cursor()
 
 re_date_query = re.compile(
     r"^date\s*(:|>|<|>=|<=)\s*([0-9]{4}/[0-9]{2}/[0-9]{2})(?:\s+|$)"
@@ -48,19 +54,15 @@ def filter_email(field, email):
     lookup_string = field + "-" + email
     new_rows = []
 
-    curs = emails_db.cursor()
-
-    entry = curs.set(lookup_string.encode("utf-8"))
+    entry = emails_curs.set(lookup_string.encode("utf-8"))
     while entry is not None:
         new_rows.append(entry[1])
-        entry = curs.next_dup()
+        entry = emails_curs.next_dup()
 
     if current_rows is None:
         current_rows = new_rows
     else:
         current_rows = intersect(current_rows, new_rows)
-
-    curs.close()
 
 
 def filter_field(field, term, is_prefix):
@@ -79,25 +81,23 @@ def filter_field(field, term, is_prefix):
     lookup_string = ("b" if field == "body" else "s") + "-" + term
     new_rows = []
 
-    curs = terms_db.cursor()
-
     if is_prefix:
-        entry = curs.set_range(lookup_string.encode("utf-8"))
-        while entry is not None and str(entry[0].decode("utf-8")).startswith(lookup_string):
+        entry = terms_curs.set_range(lookup_string.encode("utf-8"))
+        while entry is not None and str(entry[0].decode("utf-8")).startswith(
+            lookup_string
+        ):
             new_rows.append(entry[1])
-            entry = curs.next()
+            entry = terms_curs.next()
     else:
-        entry = curs.set(lookup_string.encode("utf-8"))
+        entry = terms_curs.set(lookup_string.encode("utf-8"))
         while entry is not None:
             new_rows.append(entry[1])
-            entry = curs.next_dup()
+            entry = terms_curs.next_dup()
 
     if current_rows is None:
         current_rows = new_rows
     else:
         current_rows = intersect(current_rows, new_rows)
-
-    curs.close()
 
 
 def show_records():
@@ -166,13 +166,22 @@ def main():
             command = input("> ")
             parse(command)
     except KeyboardInterrupt:
-        print()
-        print()
-        print("See you next time!")
+        pass
+
+    print()
+    print()
+    print("See you next time!")
+
+    recs_db.close()
+
+    terms_curs.close()
+    terms_db.close()
+
+    emails_curs.close()
+    emails_db.close()
+
+    dates_curs.close()
+    dates_db.close()
 
 
 main()
-recs_db.close()
-terms_db.close()
-emails_db.close()
-dates_db.close()
