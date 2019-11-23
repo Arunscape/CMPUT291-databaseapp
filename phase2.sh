a885d6c936d9de4a89060e856c08061513a35258
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-23 13:02:41 -0700

    fix https://github.com/jspenguin2017/database-app/issues/3

--- phase2.sh
+++ phase2.sh
@@ -8,9 +8,9 @@ rm -f te.idx
 rm -f em.idx
 rm -f da.idx 
 
-sort -u recs.txt | ./break.pl | db_load -T -t hash re.idx &
-sort -u terms.txt | ./break.pl | db_load -c duplicates=1 -T -t btree te.idx &
-sort -u emails.txt | ./break.pl | db_load -c duplicates=1 -T -t btree em.idx &
-sort -u dates.txt | ./break.pl | db_load -c duplicates=1 -T -t btree da.idx &
+sort -u recs.txt   | perl break.pl | db_load -T -t hash re.idx &
+sort -u terms.txt  | perl break.pl | db_load -c duplicates=1 -T -t btree te.idx &
+sort -u emails.txt | perl break.pl | db_load -c duplicates=1 -T -t btree em.idx &
+sort -u dates.txt  | perl break.pl | db_load -c duplicates=1 -T -t btree da.idx &
 
 wait
